87ebe35d04bf0a3818ba4b50fd8125847fc1696b
Hackerzone <101497469+Itgeeksk@users.noreply.github.com>
2022-08-04 12:54:14 +0530

    Improving Weather app

--- main/wthap/views.py
+++ main/wthap/views.py
@@ -19,14 +19,15 @@ def HomeView(request):
             data = json.loads(req)
             context['city'] = city
             context['code'] = str(data['cod']) 
-            context["temp"] = str(round(data['list'][2]['main']["temp"] - 273.15, 2)) + " Celsius"
-            context["pressure"] = str(data['list'][2]['main']['pressure'])
+            context["temp"] = str(round(data['list'][0]['main']["temp"] - 273.15, 1)) + " Celsius"
+            context["pressure"] = str(data['list'][0]['main']['pressure'])
+            context['tempmin'] =  str(round(data['list'][0]['main']["temp_min"] - 273.15, 1)) + " Celsius"
+            context['tempmax'] = str(round(data['list'][0]['main']["temp_max"] - 273.15, 1)) + " Celsius"
+            context['wind'] = str(data['list'][0]['wind']['speed'])
+            context['weather'] = str(data['list'][0]['weather'][0]['main'])
+            context['description'] = str(data['list'][0]['weather'][0]['description'])
+            context['stats'] = str(data['list'][1])
     else:
         city = ""
-        context = {
-            "city" : " - " ,
-            "code" : " - " ,
-            "temp" : " - ",
-            "pressure" : " - ",
-        }
+        context = {}
     return render(request, 'index.html', context)
