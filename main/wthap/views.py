b5d8fadfe041eb0e497b84f2258a5886f673545a
Hackerzone <101497469+Itgeeksk@users.noreply.github.com>
2022-08-04 13:09:49 +0530

    still making better

--- main/wthap/views.py
+++ main/wthap/views.py
@@ -9,19 +9,19 @@ def HomeView(request):
         if city == "":
             context = {}
         else:
-            url = f"https://api.openweathermap.org/data/2.5/forecast?q={city}&appid=21c2fba050f4b6c020c53a04db856bb9"
+            url = f"https://api.openweathermap.org/data/2.5/forecast?q={city}&appid={Apikey}"
             req = ur.urlopen(url).read()
             data = json.loads(req)
             context['city'] = city
             context['code'] = str(data['cod']) 
             context["temp"] = str(round(data['list'][0]['main']["temp"] - 273.15, 1)) + " Celsius"
+            context["humidity"] = str(data['list'][0]['main']['humidity']) + "%"
             context["pressure"] = str(data['list'][0]['main']['pressure'])
             context['tempmin'] =  str(round(data['list'][0]['main']["temp_min"] - 273.15, 1)) + " Celsius"
             context['tempmax'] = str(round(data['list'][0]['main']["temp_max"] - 273.15, 1)) + " Celsius"
             context['wind'] = str(data['list'][0]['wind']['speed'])
             context['weather'] = str(data['list'][0]['weather'][0]['main'])
             context['description'] = str(data['list'][0]['weather'][0]['description'])
-            context['stats'] = str(data['list'][1])
     else:
         city = ""
         context = {}
